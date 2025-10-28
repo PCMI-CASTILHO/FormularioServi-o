e4bbba0188fe3d073c6e297e010bfd7ebfedd620
PCMI-CASTILHO <pcmipesoexatobalancas@gmail.com>
2025-10-28 11:05:55 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 importScripts('https://cdn.jsdelivr.net/npm/idb@8/build/umd.js');
 // Nome do cache — altere sempre que atualizar
-const CACHE_NAME = 'formulario-cache-v49';
+const CACHE_NAME = 'formulario-cache-v50';
 
 // Arquivos para cache inicial - URLs ABSOLUTAS
 const ASSETS_TO_CACHE = [
